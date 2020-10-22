a08904113f537aabbde492c4835ab539d1c7bb5b
hgc <hgc@18join.com>
2020-10-22 10:08:06 +0800

    add:lvst#15

--- core/callbacks/bestkeepcallback.py
+++ core/callbacks/bestkeepcallback.py
@@ -19,4 +19,4 @@ class BestKeepCheckpoint(tf.keras.callbacks.Callback):
             save_path = self.save_path.format(epoch=epoch)
             print("save model " + save_path)
             self.eval_model.save(save_path)
-            shutil.copy(save_path, "/content/drive/My\ Drive/ICDAR_2019_LSVT/")
+            shutil.copy(save_path, "/content/drive/My Drive/ICDAR_2019_LSVT/")
